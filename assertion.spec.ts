54523318019cf645367018c461ee089219845bb1
sarmadbaig93 <sarmadbaig93@gmail.com>
2023-12-19 15:53:29 +0100

    modified asssertion.spec.ts | deleted example.spec.ts

--- assertion.spec.ts
+++ assertion.spec.ts
@@ -1,13 +1,15 @@
 import test, {Page, expect} from '@playwright/test'
 test('assertion demo', async({page}) =>{
+    
+    // check assertion on login page
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     await page.pause()
     await expect(page.getByRole('heading', {name:'Login'})).toHaveText('Login')
     await page.pause()
+    // check element present or not
     await expect(page.getByText('username')).toHaveCount(2)
     await page.pause()
     await expect(page.getByText('password')).toHaveCount(3)
-    await page.pause()
 
 }
 )
